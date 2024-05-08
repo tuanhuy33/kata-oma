5a4de4749a043b8f23ad1934c2fc11c0f685c1aa
tuanhuy33 <tuanhuy6933@gmail.com>
2024-05-08 10:26:18 +0700

    update

---
+++ Test Suites/New Test Suite (2).ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite (2)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5bb1fbd1-560a-41fd-98f3-d2c9bcdc9d9e</testSuiteGuid>
+   <testCaseLink>
+      <guid>bffdeb40-61de-479e-80ad-91affd6c40b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/da</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
