cf9b1dedddd0e904a067c8cf25a0cfeaed5abb9e
tuanhuy33 <tuanhuy6933@gmail.com>
2024-05-14 10:15:58 +0700

    upadgaeeqws

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e892634e-bc6a-4210-af94-4a38e3d53c31</testSuiteGuid>
+</TestSuiteEntity>
