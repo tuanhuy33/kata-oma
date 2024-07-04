279b28fb2eab566b63c6dc4a378b2f6acbce774d
tuanhuy33 <tuanhuy6933@gmail.com>
2024-07-04 13:22:07 +0700

    asda

--- Test Suites/thang/New Dynamic Test Suite.ts
+++ Test Suites/thang/New Dynamic Test Suite.ts
@@ -14,5 +14,5 @@
    <filteringBuiltIn>com.kms.katalon.execution.platform.DynamicBuiltInSearch</filteringBuiltIn>
    <filteringExtension></filteringExtension>
    <filteringPlugin></filteringPlugin>
-   <filteringText>name=(New) </filteringText>
+   <filteringText>name=(abc) </filteringText>
 </FilteringTestSuiteEntity>
